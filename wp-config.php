c07943a2e5e89308a6115810cce65ce9d3efeaa6
13485 <yunqiang.wu@hand-china.com>
2017-05-09 19:33:59 +0800

    'add_gitignore'

--- wp-config.php
+++ wp-config.php
@@ -30,7 +30,7 @@ define('DB_NAME', 'wordpress');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'abc123');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', '127.0.0.1');
