e65d0121ee6d80cd5b245ecc3698a87c65e5bc67
HJW <hwacer@outlook.com>
2017-06-05 21:45:13 +0800

    优化首页样式

--- wp-content/themes/begin/pages/template-padagogy.php
+++ wp-content/themes/begin/pages/template-padagogy.php
@@ -4,7 +4,11 @@ Template Name: Padagogy
 */
 ?>
 <?php get_header(); ?>
-
+<style>
+    .tag-groups-cloud-tabs .ui-widget-header .ui-state-default{
+        border:none;
+    }
+</style>
     <!-- 主页工具 -->
     <div id="cms-widget-one" class="wow fadeInUp" data-wow-delay="0.5s">
         <?php if ( ! dynamic_sidebar( 'cms-index' ) ) : ?>
@@ -37,6 +41,11 @@ Template Name: Padagogy
                 ?>
             <?php } ?>
 
+            <aside id="php_text-5" class="widget php_text wow fadeInUp" data-wow-delay="0.3s" style="margin-top: 15px;height: 42px;">
+                <h3 class="widget-title" style="line-height: 40px;"><i class="fa fa-bars"></i>按标签浏览</h3>
+                <div class="textwidget widget-text"></div>
+                <div class="clear"></div>
+            </aside>
             <?php
             if ( function_exists( 'tag_groups_cloud' ) ) echo tag_groups_cloud( array( 'amount' => 10 ) );
             ?>
